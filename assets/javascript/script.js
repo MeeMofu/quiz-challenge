7918b33c1f2f06ffa75a3f67dafbe8df121be084
VTran <MeeMofu@users.noreply.github.com>
2020-08-28 13:04:07 -0400

    Added input pannels + prototyping JS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,16 @@
 var startButton = document.querySelector("#start-btn");
 var info = document.querySelector("#info");
+var userIput =document.querySelector("#quiz-input")
+
 startButton.addEventListener("click",function(event){
     info.classList.add("hide");
     startButton.classList.add("hide");
+    userIput.classList.remove("hide");
+})
+
+userIput.addEventListener("click",function(event){
+    var option = event.target.closest(".option-wrapper");
+    if (option){
+        console.log(option.querySelector(".option").getAttribute("data-choice-id"));
+    }
 })
